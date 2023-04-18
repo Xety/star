c81a8f553bcf7fc9a9a8e48848328a6d74531fc3
Emeric Fevre <emeric@xetaravel.com>
2023-04-18 15:50:16 +0200

    Added Flash messages

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import './bootstrap';
 
+import Dismiss from './libs/dismiss.js';
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,10 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
+
+        <!-- Flash Messages -->
+        @include('elements.flash')
+
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
